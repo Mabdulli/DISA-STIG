9092a2a6ea0ab6411c030f8f410c7d4a4143fda4
Mabdulli <byussuf254@gmail.com>
2025-07-06 17:19:37 -0500

    Update WN10-CC-000025.ps1

--- STIGS/WN10-CC-000025.ps1
+++ STIGS/WN10-CC-000025.ps1
@@ -13,6 +13,11 @@
     Plugin IDs      : N/A
     STIG-ID         : WN10-CC-000025
 
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
 
 .USAGE
     Put any usage instructions here.
